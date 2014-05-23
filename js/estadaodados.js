3c5025237fb4bad30168220fcd8c10318c9882c8
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2014-05-23 18:05:54 -0300

    Atualizando

--- js/estadaodados.js
+++ js/estadaodados.js
@@ -1,56 +1,61 @@
-//$(".porcentagens input").slider();
+var referencias = {
+    'sim_dilma': 38,
+    'nao_dilma': 32
+}
 
-var currentValues = {
-    'OtB': 0,
-    'R': 0
-};
+var conversao = {
+    'OtB': 0.6,
+    'R': 0.3,
+    'RP': 0.1,
+    'BN': 0.3
+}
 
 $(document).ready(function(){
-
-
-    function atualizaSmall(otbValue, rValue, rpValue) {
-        $(".valorOtB small").text(otbValue);
-        $(".valorR small").text(rValue);
-        $(".valorRP small").text(rpValue);
-    }
-    var ChangeInt = function() {
-        console.log("teste2");
-        if (RP.slider('getValue') > 0) {
-            RP.slider('setValue',(100 - OtB.slider('getValue') - R.slider('getValue')));
+  function atualizaSmall(otbValue, rValue, rpValue) {
+    $(".valorOtB small").text(otbValue);
+    $(".valorR small").text(rValue);
+    $(".valorRP small").text(rpValue);
+  }
+  function changeInt(event) {
+    if (event.currentTarget.id == "OtB") {
+        if (event.value + R.slider('getValue') > 100) {
+            R.slider('setValue', (100 - event.value));
         } else {
-            if (currentValues[this.id] <= this.value) {
-                if (this.id == "OtB") {
-                    R.slider('setValue',(100 - OtB.slider('getValue')));
-                } else {
-                    OtB.slider('setValue',(100 - R.slider('getValue')));
-                }
-            } else {
-                RP.slider('setValue',(100 - OtB.slider('getValue') - R.slider('getValue')));
-            }
+            RP.slider('setValue',(100 - event.value - R.slider('getValue')));
         }
-        currentValues[this.id] = this.value;
-        atualizaSmall(OtB.slider('getValue'), R.slider('getValue'), RP.slider('getValue'));
-    };
+    } else {
+        if (event.value + OtB.slider('getValue') >= 100) {
+            OtB.slider('setValue', (100 - event.value));
+        } else {
+            RP.slider('setValue',(100 - event.value - OtB.slider('getValue')));
+        }
+    }
+    atualizaSmall(OtB.slider('getValue'), R.slider('getValue'), RP.slider('getValue'));
+  };
 
-    function DispatchSlide() {
-        this.trigger({
-                'type': 'slide',
-                'value': slideEventValue
-            })
-            .data('value', this.value)
-            .prop('value', this.value);
-            console.log("teste")
-    };
+  var RP = $('#RP').slider({});
+  var BN = $('#BN').slider({});
+  var OtB = $('#OtB').slider({})
+        .on('slideStart', function(event){changeInt(event);})
+        .on('slide', function(event){changeInt(event);});
+  var R = $('#R').slider({})
+        .on('slideStart', function(event){changeInt(event);})
+        .on('slide', function(event){changeInt(event);});
 
-    var RP = $('#RP').slider({});
-    var OtB = $('#OtB').slider({})
-            .on('slide', ChangeInt);
-    $('#OtB').slider().click(function(){alert("teste")});
-    var R = $('#R').slider({})
-            .on('slide', ChangeInt)
-            .on('touch', ChangeInt)
-            .on('click', DispatchSlide);
-    $(".valorOtB small").text(OtB.slider('getValue'));
-    $(".valorR small").text(R.slider('getValue'));
-    $(".valorRP small").text(RP.slider('getValue'));
+  $(".valorOtB small").text(OtB.slider('getValue'));
+  $(".valorR small").text(R.slider('getValue'));
+  $(".valorRP small").text(RP.slider('getValue'));
+
+    var tangle = new Tangle(document,{
+        initialize: function() {
+
+        },
+        update: function() {
+          console.log(this)
+          conversao_dilma = OtB.slider('getValue') * 1;
+          console.log(conversao_dilma);
+          this.sim_dilma = referencias.sim_dilma + conversao_dilma * BN.slider('getValue');
+          this.nao_dilma = referencias.nao_dilma + BN.slider('getValue') * (1 - conversao_dilma);
+        }
+    })
 });
